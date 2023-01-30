5252f4133a0dd75c6d1d6c0f44d203d01bfb797f
Everton Leão <everleao@gmail.com>
2023-01-29 23:27:27 -0300

    Add project files.

---
+++ FiltroGitHubASP/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FiltroGitHubASP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ FiltroGitHubASP/wwwroot/js/repogit.js
@@ -0,0 +1,35 @@
+﻿const repositorios = document.querySelector('.content-main');
+
+function getApiGitHub() {
+    fetch('https://api.github.com/users/VToum/repos')
+        .then(async res => {
+            if (!res.ok) {
+                throw new Error(res.status);
+            }
+
+            let data = await res.json();
+            console.log(data);
+            data.map(item => {
+                let project = document.createElement('div');
+
+                project.innerHTML = `
+                       <div class="project">
+                          <div>
+                           <h3 class="title">${ item.name}</h3>
+                           <h6 class="title">${ item.description}</h5>
+                           <span class="data-create">${ Intl.DateTimeFormat('pt-BR').format(new Date(item.created_at)) }</span>
+                        </div>
+                            <div>
+                                <a href="${item.html_url}" target="_blank">${item.html_url}</a>
+                                <span class="language"><span class="circle"></span>${item.language}</span>
+                        </div>
+                         </div>
+                `
+
+                repositorios.appendChild(project);
+            })
+        })
+
+}
+
+getApiGitHub();
